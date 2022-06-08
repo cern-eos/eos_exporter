8648ad1bd905b214e3315649bb30550a1f14799c
Aritz Brosa <aritz.brosa.iartza@cern.ch>
2022-06-08 11:54:04 +0200

    Change check output of ListIOInfo

--- collector/io.go
+++ collector/io.go
@@ -276,6 +276,9 @@ func (o *IOInfoCollector) collectIOInfoDF() error {
 	mds, err := client.ListIOInfo(context.Background())
 	if err != nil {
 		fmt.Println("Panic error while ListIOInfo: ", err)
+		for _, m := range mds {
+			fmt.Println("Measurement: ", m.Measurement)
+		}
 		panic(err)
 	}
 
