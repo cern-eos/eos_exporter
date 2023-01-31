dd8acd54f14c73dedab404c2669aa6c154cb1ce2
Hugo Labrador <labkode@users.noreply.github.com>
2023-01-31 16:56:07 +0100

    Update who.go

--- collector/who.go
+++ collector/who.go
@@ -17,7 +17,7 @@ import (
 // uid=982 nsessions=2
 // uid=983 nsessions=2
 // c) Client info
-// client=akehrli@cbox-lbeosgw-10.cern.ch uid=akehrli auth=https idle=66 gateway="cbox-lbproxy-05.cern.ch" app=http
+// client=yyy@xxx.cern.ch uid=yyy auth=https idle=66 gateway="xxx.cern.ch" app=http
 // Because a) and b) can be derived from c), we only report c) in the metric
 // Aggregation on fields from c) can be done in the monitoring system
 // This collector provides metrics based on c)
