11e6daaeca41890c807c20e3643c3e65b0358304
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2026-03-19 11:13:47 +0100

    fix binary version issue

--- get_build_info.sh
+++ get_build_info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git rev-parse --short HEAD | tr -d "\n" > .git_commit
 date +%FT%T%z | tr -d "\n" > .build_date
-git describe --always | tr -d "\n" > .version
+git describe --tags --always | tr -d "\n" > .version
 go version | awk '{print $3}' | tr -d "\n" > .go_version
